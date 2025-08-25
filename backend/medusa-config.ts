7812d7690ffc01a23237d96f5211dc2ce49f7bc9
phryll <philipp_heider@yahoo.de>
2025-08-25 15:23:11 +0200

    Update medusa-config.ts

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -8,7 +8,6 @@ loadEnv(process.env.NODE_ENV!, process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
